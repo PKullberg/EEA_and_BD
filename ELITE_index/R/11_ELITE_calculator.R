fb79fe129dfa154d32d482271799ad4a444d4804
Peter Kullberg <peter.kullberg@ymparisto.fi>
2020-07-07 15:43:35 +0300

    small fixes, typos etc

--- ELITE_index/R/11_ELITE_calculator.R
+++ ELITE_index/R/11_ELITE_calculator.R
@@ -13,13 +13,13 @@ library(dplyr)
 reference_variables <- list(
   kangas = c(dead_wood = 80, large_trunks = 20, broad_leaved = 50),
   lehto = c(dead_wood = 100, large_trunks = 30, broad_leaved = 100),
-  karukko = c(dead_wood = 40, large_trunks = 10, burnt_area = 5000)
+  karukko = c(dead_wood = 40, large_trunks = 10, burnt_area = 1) # Burnt are is just a dummy value that should not be used
 )
 
 feature_weights <- list(
   kangas = c(dead_wood = 0.6, large_trunks = 0.4, broad_leaved = 0.4),
   lehto = c(dead_wood = 0.4, large_trunks = 0.4, broad_leaved = 0.6),
-  karukko = c(dead_wood = 0.4, large_trunks = 0.4, burnt_area = 0.5)
+  karukko = c(dead_wood = 0.4, large_trunks = 0.4, burnt_area = 0) # Burnt are now with weight 0 as there is no good data avaialble 
 )
 
 # Function for calcualting the condition index
@@ -37,8 +37,8 @@ ELITE_value <- function(cur_values, habitat, reference_variable_list = reference
   ref_values <- reference_variable_list[[habitat]]
   weights <- weight_list[[habitat]]
   
-  # if inputs exceed reference values replace them with reference
-  if(exceed_warnings) if(any(cur_values > ref_values)) warning("One or more input values exceed the reference value. Replacing exceeding values with reference value.\n")
+  # if inputs exceed reference values replace them with the reference value
+  if(exceed_warnings) if(any(cur_values > ref_values)) warning("One or more input values exceeds the reference value. Replacing exceeding values with reference value.\n")
   cur_values[cur_values > ref_values] <- ref_values[cur_values > ref_values]
   
   # compute the ELITE value
@@ -46,15 +46,10 @@ ELITE_value <- function(cur_values, habitat, reference_variable_list = reference
   
 }
 
-# this just a simple vrapper that vectorizes the ELITE_value function 
+# this just a simple wrapper that vectorizes the ELITE_value function 
 ELITE_calculator <- function(cur_values, habitat, reference_variable_list = reference_variables, weight_list = feature_weights) {
   apply(cur_values, 1, function(x) ELITE_value(x, habitat = habitat, reference_variable_list = reference_variable_list, weight_list = weight_list))
 }
-
-# Some exmpales 
-ELITE_value(cur_values = c(dead_wood = 80, large_trunks = 20, broad_leaved = 50), habitat = "kangas")
-ELITE_value(cur_values = c(dead_wood = 10, large_trunks = 0, broad_leaved = 0), habitat = "kangas")
-
 
 
 
