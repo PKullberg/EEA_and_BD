17ceed6ab0a382bc8db77ad3de2b6bf00dfc9915
Peter Kullberg <peter.kullberg@ymparisto.fi>
2020-07-08 17:29:10 +0300

    added weights to be used with scaled and linearized elite index calculators

--- ELITE_index/R/11_ELITE_calculator.R
+++ ELITE_index/R/11_ELITE_calculator.R
@@ -16,10 +16,27 @@ reference_variables <- list(
   karukko = c(dead_wood = 40, large_trunks = 10, burnt_area = 1) # Burnt are is just a dummy value that should not be used
 )
 
+# the original weights
 feature_weights <- list(
   kangas = c(dead_wood = 0.6, large_trunks = 0.4, broad_leaved = 0.4),
   lehto = c(dead_wood = 0.4, large_trunks = 0.4, broad_leaved = 0.6),
   karukko = c(dead_wood = 0.4, large_trunks = 0.4, burnt_area = 0) # Burnt are now with weight 0 as there is no good data avaialble 
+)
+
+# weights to be used with the scaled metric
+# the aim is to retain the the original interpretation of the weights
+# original interpretation: "if feature is comletely missing and others at maximum the elite value should be 1 - weight (original)"
+feature_weights_s <- list(
+  kangas = c(dead_wood = 0.419, large_trunks = 0.279, broad_leaved = 0.279),
+  lehto = c(dead_wood = 0.279, large_trunks = 0.279, broad_leaved = 0.419),
+  karukko = c(dead_wood = 0.232, large_trunks = 0.232, burnt_area = 0.29)
+)
+
+# weights to be used with the linearized metric to retain the original interpretation of weight
+feature_weights_l <- list(
+  kangas = c(dead_wood = 0.81, large_trunks = 0.64, broad_leaved = 0.64),
+  lehto = c(dead_wood = 0.64, large_trunks = 0.64, broad_leaved = 0.81),
+  karukko = c(dead_wood = 0.52, large_trunks = 0.52, burnt_area = 0.62)
 )
 
 # Function for calcualting the condition index
@@ -69,7 +86,7 @@ ELITE_value_scaled <- function(cur_values, habitat, reference_variable_list = re
 }
 
 # this value linearizes (having all variables at 0.5 gives index close to 0.5) the original elite index and scales it range from 0 to 1
-ELITE_value_linear <- function(cur_values, habitat, reference_variable_list = reference_variables, weight_list = feature_weights, exceed_warnings = T) {
+ELITE_value_linear <- function(cur_values, habitat, reference_variable_list = reference_variables, weight_list = feature_weights_l, exceed_warnings = T) {
   
   stopifnot(habitat %in% names(reference_variable_list))
   
@@ -81,11 +98,10 @@ ELITE_value_linear <- function(cur_values, habitat, reference_variable_list = re
   if(exceed_warnings) if(any(cur_values > ref_values)) warning("One or more input values exceeds the reference value. Replacing exceeding values with reference value.\n")
   cur_values[cur_values > ref_values] <- ref_values[cur_values > ref_values]
   
-  scaler <- prod(1 - weights)^(1/length(cur_values))
+  scaler <- prod(1 - weights)^(1 / length(cur_values))
   
   # compute the ELITE value
-  ((prod(1 - weights * (1 - cur_values / ref_values)))^(1/length(cur_values)) - scaler) / (1 - scaler)
-  
+  ((prod(1 - weights * (1 - cur_values / ref_values)))^(1 / length(cur_values)) - scaler) / (1 - scaler)
 }
 
 
